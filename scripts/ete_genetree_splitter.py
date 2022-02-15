5b6334b64385d242f1d535c7c828c1e5f59eac7b
Anil Thanki (EI) <thankia@v0768.nbi.ac.uk>
2022-02-15 09:41:05 +0000

    adds pipline supporting scripts

---
+++ scripts/ete_genetree_splitter.py
@@ -0,0 +1,87 @@
+from __future__ import print_function
+
+import optparse
+import os
+
+from ete3 import PhyloTree
+
+
+def main():
+    usage = "usage: %prog --genetree <genetree-file> --speciestree <speciestree-file> [options]"
+    parser = optparse.OptionParser(usage=usage)
+    parser.add_option('--genetree', help='GeneTree in nhx format')
+    parser.add_option('--speciestree', help='Species Tree in nhx format')
+    parser.add_option('--species_format', type='int', default=8, help='Species Tree input format (0-9)')
+    parser.add_option('--gene_node', type='int', default=0, help='Gene node format 0=gene_species, 1=species_gene')
+    parser.add_option('--gainlose', action='store_true', default=False, help='Find out gene gain/lose')
+    parser.add_option('--split', type='choice', choices=['dups', 'treeko'], dest="split", default='dups', help='Choose GeneTree splitting algorithms')
+    parser.add_option('--output_format', type='int', default=9, help='GeneTree output format (0-9)')
+    parser.add_option('-d', '--dir', type='string', default="", help="Absolute or relative path to output directory. If directory does not exist it will be created") 
+    options, args = parser.parse_args()
+
+    if options.dir != "" and not os.path.exists(options.dir):
+        os.makedirs(options.dir)
+
+    if options.genetree is None:
+        parser.error("--genetree option must be specified, GeneTree in nhx format")
+
+    if os.stat(options.genetree).st_size == 0:
+        exit()
+ 
+    with open(options.genetree, 'r') as f:
+        contents = f.read()
+
+    # Remove empty NHX features that can be produced by TreeBest but break ete3
+    contents = contents.replace('[&&NHX]', '')
+
+    # reads single gene tree
+    genetree = PhyloTree(contents)
+
+    # sets species naming function
+    if options.gene_node == 0:
+        genetree.set_species_naming_function(parse_sp_name)
+
+    # reconcile species tree with gene tree to help find out gene gain/lose
+    if options.gainlose:
+
+        if options.speciestree is None:
+            parser.error("--speciestree option must be specified, species tree in nhx format")
+
+        # reads species tree
+        speciestree = PhyloTree(options.speciestree, format=options.species_format)
+
+        # Removes '*' from Species names comes from Species tree configrured for TreeBest
+        for leaf in speciestree:
+            leaf.name = leaf.name.strip('*')
+
+        genetree, events = genetree.reconcile(speciestree)
+
+    if options.split == "dups":
+        # splits tree by duplication events which returns the list of all subtrees resulting from splitting current tree by its duplication nodes.
+        for cluster_id, node in enumerate(genetree.split_by_dups(), 1):
+            outfile = str(cluster_id) + '_genetree.nhx'
+            if options.dir != "":
+                outfile = options.dir+"/"+str(cluster_id) + '_genetree.nhx'
+            with open(outfile, 'w') as f:
+                f.write(node.write(format=options.output_format))
+    elif options.split == "treeko":
+        # splits tree using the TreeKO algorithm.
+        ntrees, ndups, sptrees = genetree.get_speciation_trees()
+
+        cluster_id = 0
+        for spt in sptrees:
+            cluster_id = cluster_id + 1
+            outfile = str(cluster_id) + '_genetree.nhx'
+            if options.dir != "":
+                outfile = options.dir+"/"+str(cluster_id) + '_genetree.nhx'
+            with open(outfile, 'w') as f:
+                f.write(spt.write(format=options.output_format))
+
+
+def parse_sp_name(node_name):
+    return node_name.split("_")[1]
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ scripts/hcluster_sg_parser.py
@@ -0,0 +1,49 @@
+"""
+A simple parser to convert the hcluster_sg output into lists of IDs, one list for each cluster.
+
+When a minimum and/or maximum number of cluster elements are specified, the IDs contained in the filtered-out clusters are collected in the "discarded IDS" output dataset.
+
+Usage:
+
+python hcluster_sg_parser.py [-m <N>] [-M <N>] <file> <discarded_out>
+"""
+import optparse
+import sys
+import os
+
+
+
+def main():
+    parser = optparse.OptionParser()
+    parser.add_option('-m', '--min', type='int', default=0, help='Minimum number of cluster elements')
+    parser.add_option('-M', '--max', type='int', default=sys.maxsize, help='Maximum number of cluster elements')
+    parser.add_option('-d', '--dir', type='string', default="", help="Absolute or relative path to output directory. If directory does not exist it will be created") 
+    options, args = parser.parse_args()
+
+    if options.dir != "" and not os.path.exists(options.dir):
+        os.mkdir(options.dir)
+    with open(args[2], 'w') as discarded_max_out:
+        with open(args[1], 'w') as discarded_min_out:
+            with open(args[0]) as fh:
+                for line in fh:
+                    line = line.rstrip()
+                    line_cols = line.split('\t')
+                    cluster_id = line_cols[0]
+                    n_ids = int(line_cols[-2])
+                    id_list = line_cols[-1].replace(',', '\n')
+                    if n_ids < options.min:
+                        discarded_min_out.write(id_list)
+                    elif n_ids > options.max:
+                        discarded_max_out.write(id_list)
+                    else:
+                        outfile = cluster_id + '_output.txt'
+                        if options.dir != "":
+                            outfile = options.dir+"/"+cluster_id + '_output.txt'
+                        
+                        with open(outfile, 'w') as f:
+                            f.write(id_list)
+
+
+if __name__ == "__main__":
+    main()
+
